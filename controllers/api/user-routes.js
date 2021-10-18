89d9614f22e39b1cfabd4ca89e0431d5d79b2482
Aidan Amato <aidanamato@comcast.net>
2021-10-18 14:39:27 -0400

    add post user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
 const {User} = require('../../models');
 
+router.post('/', (req, res) => {
+  User.create(req.body)
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
