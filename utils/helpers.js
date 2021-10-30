fd091fa30d95839a61d73fb0018f0191915c6f18
Aidan Amato <aidanamato@comcast.net>
2021-10-30 19:10:54 -0400

    add 'format_plural helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,11 @@
 module.exports = {
   format_date: date => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at ${new Date(date).toLocaleString('en-US', { hour: 'numeric', minute: '2-digit'})}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`
+    }
+    return word;
   }
 }
