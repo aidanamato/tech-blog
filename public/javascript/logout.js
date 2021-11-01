117143b2d065c55e1f0e988138ba3bd5744a5dba
Aidan Amato <aidanamato@comcast.net>
2021-10-31 21:41:47 -0400

    log out user after 10 minutes of inactivity

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -12,3 +12,20 @@ async function logoutHandler() {
 }
 
 document.querySelector('#logout').addEventListener('click', logoutHandler);
+
+// logout after 10 minutes of inactivity
+// reset timer on any user activity
+window.onclick = resetTimeout;
+window.onmousemove = resetTimeout;
+window.onmousedown = resetTimeout;
+window.onkeypress = resetTimeout;
+window.onscroll = resetTimeout;
+
+// function to reset timer
+function resetTimeout() {
+  clearTimeout(timeout);
+  timeout = setTimeout(logoutHandler, 600000);
+}
+
+// initialize timer on page load
+let timeout = setTimeout(logoutHandler, 600000);
